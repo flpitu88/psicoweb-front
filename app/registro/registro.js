dfb506654b6735c0e10d36396237ef737d76f060
flpitu88 <flpitu88@hotmail.com>
2017-02-08 02:01:49 -0300

    Empiezo a armar recurso rest http que postea datos, por ahora da error.

--- app/registro/registro.js
+++ app/registro/registro.js
@@ -12,14 +12,43 @@ angular.module('myApp.registro', ['ngRoute'])
 
 // Home controller
         .controller('RegistroCtrl', function ($scope, $http) {
+
             $scope.registrar = function () {
-                $http.post({
-                    method: "POST",
-                    url: "http://localhost:8080/psicoweb/usuarios"
-                }).then(function mySuccess(response) {
 
-                }, function myError(response) {
+//                var parameter = JSON.stringify(
+//                        {type: "Usuario",
+//                            nombre: $scope.nombre,
+//                            apellido: $scope.apellido,
+//                            dni: $scope.dni,
+//                            email: $scope.email,
+//                            password: $scope.password
+//                        });
 
-                });
+                var jsonDatos = "'nombre':'flavio','apellido':'pietrolati','dni':324324,'email':'asdasd@gsg','password':'234234'";
+
+                var postConfig = {
+                    headers: {
+                        "MYAPP-DOMAIN": "http://localhost:8000",
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    },
+                    withCredentials: true
+                };
+
+                $http.post(
+                        'http://localhost:8080/psicoweb-server/usuarios',
+//                        parameter,
+                        jsonDatos,
+                        postConfig).
+                        success(function (data, status, headers, config) {
+                            // this callback will be called asynchronously
+                            // when the response is available
+                            console.log(data);
+                        }).
+                        error(function (data, status, headers, config) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                            console.log(data);
+                        });
+
             };
         });
